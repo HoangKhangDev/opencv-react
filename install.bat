843538ac499cd90394cb2f4c90435b9ed6f84817
Vũ Hoàng Khang <vuhoangkhang19032000kg@gmail.com>
2023-05-25 04:20:06 +0700

    create
    create

---
+++ install.bat
@@ -0,0 +1,10 @@
+@echo off
+
+@echo Npm Istall package
+
+npm i react@18.1 tesseract.js@3.0.3
+ && npm i
+
+@echo success 
+
+pause
---
+++ start.bat
@@ -0,0 +1,7 @@
+@ECHO OFF 
+
+ECHO opencv app install and start http://localhost:3000/
+
+npm start
+
+PAUSE
